880b290cc95fe444244b4918c7943f684179dc51
Thomas420 <thompkhakadze@gmail.com>
2022-08-27 23:58:59 +0400

    selecting random questions

--- resources/js/components/TestComponent.vue
+++ resources/js/components/TestComponent.vue
@@ -1,11 +1,20 @@
 <template>
-    <div>
-        
+    <div class="container">
+        <div>
+            <img :src="'/img/'+img+'.jpg'" alt="">
+            <p>{{question}}</p>
+        </div>
+
     </div>
 </template>
 <style>
-    div{
+    .container{
+        display: flex;
+        justify-content: center;
         background: #FF6700;
+    }
+    img{
+        width:400px;
     }
 </style>
 <script>
@@ -14,12 +23,44 @@ export default {
     data(){
         return {
             answer: null,
-            input: null
+            input: null,
+            questions: null,
+            question: '',
+            img: ''
         }
     },
     methods:{
         getQuestions(){
-            axios.get('api/get-questions').then(r => console.log(r))
+            axios.get('api/get-questions').then(r => {
+                this.questions = r.data                
+                this.getRandomQuestion()
+            })
+        },
+        calculateRandomValue(length){
+            return Math.floor(Math.random() * length)
+        },
+        getRandomQuestion(){
+            // Selecting Random Question
+            const questionPair = this.questions[this.calculateRandomValue(this.questions.length)]            
+            
+            // Removing Question from questions array
+            this.questions.splice(questionPair, 1)
+            
+
+            // selecting random value from a Question pair and assigning to a question variable
+            const question = Object.values(questionPair)[this.calculateRandomValue(Object.keys(questionPair).length)]
+            this.question = question
+
+            // Assigning img to a value
+            this.img = questionPair.en
+            // removing random value and assigning left out property to answer variable
+            delete questionPair[Object.keys(questionPair).find(key => questionPair[key] === question)]
+            this.answer = Object.values(questionPair)[0]
+        },
+        checkIfAnswerIsCorrect(){
+            if(this.input == this.answer){
+                this.getRandomQuestion()
+            }
         }
     },
     mounted(){
