1276b88fc2a0604f7eaa2c25f51f22f65c8d5165
Thomas420 <thompkhakadze@gmail.com>
2022-08-27 19:05:48 +0400

    initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+import './bootstrap';
+
+import {createApp} from 'vue'
+
+import App from './components/TestComponent.vue'
+
+createApp(App).mount("#app")
---
+++ resources/js/components/TestComponent.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <p>
+            This is a test Component
+        </p>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
